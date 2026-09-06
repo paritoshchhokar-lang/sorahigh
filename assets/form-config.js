91464991cd14208fa80aaa21668f44149ac27644
Paritosh404 <54989538+Paritosh404@users.noreply.github.com>
2026-09-06 10:24:25 +0530

    Update

--- assets/form-config.js
+++ assets/form-config.js
@@ -1,2 +1,2 @@
 /* Paste the deployed Google Apps Script web-app URL between the quotes. */
-window.SORA_HIGH_FORM_ENDPOINT = "https://script.google.com/macros/s/AKfycbx-QPSSX9fKaTlr5OON0zwULzBTLxcRTW1UMTg0xP_zzpzK6ycAuPHB21ukzEQCeizM/exec";
+window.SORA_HIGH_FORM_ENDPOINT = "https://script.google.com/macros/s/AKfycbyHMOUsDdGGOrODA4y6hJWEi9i2DgBdIklyZtgrexv-lVbNzuiQ3erf7ncD3vK7M6X5/exec";
